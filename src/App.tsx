0f3dde153a3d221aed89da3f0cf8c0fdaaacb331
tocomi <tocomi0112@gmail.com>
2025-04-08 01:14:42 +0900

    feat: 入力欄を下に固定する

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,20 @@ import { PinboardProvider } from './context/PinboardContext'
 export function App() {
   return (
     <PinboardProvider>
-      <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-4">
+      <div className="flex min-h-screen flex-col items-center bg-gray-100 p-4 pb-32">
         <header>
           <h1 className="mb-6 text-center font-bold text-4xl">Pinboard</h1>
         </header>
         <main className="flex w-full max-w-md flex-col items-center">
-          <ItemForm />
           <ItemList />
           <ToggleCompletedButton />
           <CompletedItems />
         </main>
+        <div className="fixed right-0 bottom-0 left-0 flex w-full justify-center bg-gray-100 p-4">
+          <div className="w-full max-w-md">
+            <ItemForm />
+          </div>
+        </div>
       </div>
     </PinboardProvider>
   )
